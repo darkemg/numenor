fdc3fa10ba4979e646a911f4f8ac31f2a6485dee
darke.goulart@gmail.com <Darke Goulart>
2015-12-08 17:34:03 -0200

    Desenvolvimento da classe controladora de inclusão de arquivos Javascript.

--- lib/Numenor/Html/ControleJavascript.php
+++ lib/Numenor/Html/ControleJavascript.php
@@ -12,6 +12,11 @@ class ControleJavascript extends Controle {
 	 * @var \Numenor\Html\Javascript[]
 	 */
 	protected $listaJs;
+	/**
+	 * 
+	 * @var array
+	 */
+	protected $listaArquivosIncluir;
 	
 	protected function gerarListaConcatCompact() {
 		$lista = array();
@@ -55,25 +60,54 @@ class ControleJavascript extends Controle {
 	
 	protected function minificar() {
 		//
+		$this->listaArquivosIncluir = array();
+		//
 		$listaConcatCompact = $this->gerarListaConcatCompact();
 		$nomeConcatCompact = $this->gerarNome($listaConcatCompact);
 		$minificadorConcatCompact = clone $this->minificadorJs;
-		if (!file_exists($this->diretorioOutput . \DIRECTORY_SEPARATOR . $nomeConcatCompact . '.js')) {
+		$outputConcatCompact = $this->diretorioOutput . \DIRECTORY_SEPARATOR . $nomeConcatCompact . '.js';
+		if (!file_exists($outputConcatCompact)) {
 			foreach ($listaConcatCompact as $js) {
 				$minificadorConcatCompact->add($js);
 			}
-			$minificadorConcatCompact->execute($this->diretorioOutput . \DIRECTORY_SEPARATOR . $nomeConcatCompact);
+			$minificadorConcatCompact->gzip($outputConcatCompact);
 		}
+		$this->listaArquivosIncluir[] = $outputConcatCompact;
 		unset($minificadorConcatCompact);
 		// 
 		$listaConcat = $this->gerarListaConcat();
 		$nomeConcat = $this->gerarNome($listaConcat);
-		if (!file_exists($this->diretorioOutput . \DIRECTORY_SEPARATOR . $nomeConcat . '.js')) {
+		$outputConcat = $this->diretorioOutput . \DIRECTORY_SEPARATOR . $nomeConcat . '.js';
+		if (!file_exists($outputConcat)) {
 			foreach ($listaConcat as $js) {
-				file_put_contents($this->diretorioOutput . \DIRECTORY_SEPARATOR . $nomeConcat . '.js', file_get_contents($js) . "\n", FILE_APPEND);
+				file_put_contents(
+						$outputConcat, 
+						gzencode(file_get_contents($js . "\n"), 9, FORCE_GZIP), 
+						FILE_APPEND);
 			}
 		}
+		$this->listaArquivosIncluir[] = $outputConcat;
 		// 
+		$listaCompact = $this->gerarListaCompact();
+		foreach ($listaCompact as $js) {
+			$minificadorCompact = clone $this->minificadorJs;
+			$nomeCompact = $this->gerarNome(array($js));
+			$outputCompact = $this->diretorioOutput . \DIRECTORY_SEPARATOR . $nomeCompact . '.js';
+			if (!file_exists($outputCompact)) {
+				$minificadorCompact->add($js);
+				$minificadorCompact->gzip($outputCompact);
+			}
+			$this->listaArquivosIncluir[] = $outputCompact;
+			unset($minificadorCompact);
+		}
+		$listaNormal = $this->gerarListaNormal();
+		foreach ($listaNormal as $js) {
+			if ($js instanceof JavascriptRemoto) {
+				
+			} else {
+				$this->listaArquivosIncluir[] = (string) $js;
+			}
+		}
 	}
 	
 	public function adicionarJs(Javascript $js) {
